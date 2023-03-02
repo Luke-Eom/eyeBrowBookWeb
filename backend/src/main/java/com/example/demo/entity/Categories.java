ddf15972ed54ffc0b84a6ec20913a8631c34c0e4
LukeEom <eluke1028@gmail.com>
2023-03-02 22:10:32 +0900

    backend - setting   (mariadb connection,build.gradle, app.props) - basic format (Entity, repo, Controller)   --dto, service required

---
+++ backend/src/main/java/com/example/demo/entity/Categories.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+
+@Entity
+@Getter
+@Table(name="categories")
+public class Categories {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 20, nullable = false)
+    private String name;
+}
