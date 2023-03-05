361b064f8d3f8def2b1257c6bb9f10657c511a31
LukeEom <eluke1028@gmail.com>
2023-03-05 23:08:28 +0900

    todo //

---
+++ backend/src/main/java/com/example/demo/controller/ScheduleController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("schedule")
+public class ScheduleController {
+
+    // date별 스케쥴 find and sortByTime
+    // GetMapping
+
+    // create
+    // if (time에 스케쥴이 == null) => exception
+
+    // delete
+
+    // update
+    // if (time에 스케쥴이 == null) => exception
+    // else update
+
+}
