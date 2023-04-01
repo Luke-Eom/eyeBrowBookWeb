749d53b41a41db1375b150fcef59ecc4f9d17b1a
LukeEom <eluke1028@gmail.com>
2023-04-01 10:48:20 +0900

    login progress (v2) - auth token (JWT) - api : user(entity, repo)

--- backend/src/main/java/com/example/demo/oauth/exception/TokenValidationFailedException.java
+++ backend/src/main/java/com/example/demo/oauth/exception/TokenValidationFailedException.java
@@ -3,9 +3,13 @@ package com.example.demo.oauth.exception;
 public class TokenValidationFailedException extends RuntimeException {
 
     public TokenValidationFailedException() {
+
         super("Failed to generate Token.");
     }
 
     private TokenValidationFailedException(String message) {
+
         super(message);
+    }
+
 }
