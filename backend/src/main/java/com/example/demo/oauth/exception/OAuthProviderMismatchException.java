7ae28df52b3757e6a9bb602fb558e3f77a4e59dc
LukeEom <eluke1028@gmail.com>
2023-04-02 22:47:11 +0900

    login progress (v2) -exception: OAuth2 pType matching exception             RestAuthenticationEntryPoint exception (check 1 more time)

---
+++ backend/src/main/java/com/example/demo/oauth/exception/OAuthProviderMismatchException.java
@@ -0,0 +1,8 @@
+package com.example.demo.oauth.exception;
+
+public class OAuthProviderMismatchException extends RuntimeException{
+
+    public OAuthProviderMismatchException(String message) {
+        super(message);
+    }
+}
