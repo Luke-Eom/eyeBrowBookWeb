65ce36f438b3df9f0f24f6c2c6b123bb8b38993a
LukeEom <eluke1028@gmail.com>
2023-04-08 22:59:35 +0900

    login progress (v2) API part -Controller: user Controller
    -Common: ApiResponse<T>, ApiResponseHeader

---
+++ backend/src/main/java/com/example/demo/api/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.api.user.controller;
+
+import com.example.demo.api.user.dto.UserDto;
+import com.example.demo.api.user.entity.MyUser;
+import com.example.demo.api.user.service.UserService;
+import com.example.demo.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{username}")
+    public ApiResponse<UserDto> getUser(MyUser myUser) {
+
+        UserDto user = userService.getUser(myUser.getEmail());
+
+        return ApiResponse.success("user", user);
+
+    }
+
+}
---
+++ backend/src/main/java/com/example/demo/common/ApiResponseHeader.java
@@ -0,0 +1,16 @@
+package com.example.demo.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponseHeader {
+
+    private int code;
+
+    private String message;
+
+}
